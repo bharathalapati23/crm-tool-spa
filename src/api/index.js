db0c433798d8669d384fee9eee4b0f94df99f162
Vishnu Reddy <cvishnureddy5500@gmail.com>
2021-12-04 17:10:32 +0530

    changed url from dev server to heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/enquiry";
-// const url = 'https://crm-tool-backend.herokuapp.com/enquiry'
+// const url = "http://localhost:5000/enquiry";
+const url = "https://crm-tool-backend.herokuapp.com/enquiry";
 
 export const createEnquiry = (newPost) => {
   console.log(newPost);
